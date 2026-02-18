871d73d9508f67c21a3df8c933521e82191f8839
Zahra <291234050@formaite.fccollege.edu.pk>
2026-02-18 16:15:14 +0500

    Add code for blastoff.

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 # # hello .py
 print("Hello from my first GitHub repo!")
 print("This is Zahra")
-for x in range(2):
+for i in range(2,-1):
     print(i)
     i-=1
+    if i==0:
+        print("Blastoff!")
